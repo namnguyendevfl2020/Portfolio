b15f1f8cc07070249c7cf51199e2fbde7c768fc3
namnguyendevfl <namnguyenfl990@gmail.com>
2022-02-13 20:54:33 -0500

    updated a restaurant image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-
 require('dotenv').config();
 
 const {
@@ -9,27 +7,9 @@ const {
 
 module.exports =() => {
     return {
-    serverRuntimeConfig: {
-      APP_EMAIL_USERNAME,
-      APP_EMAIL_PW,
-    },
-    publicRuntimeConfig: {
-        apiUrl: process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
-    },
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            // reset users json on codesandbox every 10 minutes because 
-            // the same json data is viewed by all users
-            setInterval(() => {
-                const defaultUsers = [];
-                fs.writeFileSync('data/users.json', JSON.stringify(defaultUsers, null, 4));
-                console.log('users reset to default in next.config.js');
-            }, 10 * 60 * 1000);
-        }
-
-        return config;
-    }
+        serverRuntimeConfig: {
+        APP_EMAIL_USERNAME,
+        APP_EMAIL_PW,
+        },
     }
 }
